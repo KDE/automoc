6abca8eb52509b218d41c4d00fe56c74ca3331d6
Alexander Neundorf <neundorf@kde.org>
2009-05-08 20:33:00 +0000

    -adapt this to the new enhanced KDECTestNightly.cmake script (need current kdesdk for running Nightly builds)
    Alex
    
    
    
    svn path=/trunk/kdesupport/automoc/; revision=965402

--- Automoc4Nightly.cmake
+++ Automoc4Nightly.cmake
@@ -1,17 +1,28 @@
-set(CTEST_VCS svn)
-set(CTEST_VCS_REPOSITORY https://svn.kde.org/home/kde/trunk/kdesupport/automoc)
+set(KDE_CTEST_VCS svn)
+set(KDE_CTEST_VCS_REPOSITORY https://svn.kde.org/home/kde/trunk/kdesupport/automoc)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles" )
 
 # set(CTEST_AVOID_SPACES FALSE)
 
-get_filename_component(_currentDir "${CMAKE_CURRENT_LIST_FILE}" PATH)
+# generic support code, provides the kde_ctest_setup() macro, which sets up everything required:
+file(TO_CMAKE_PATH  $ENV{KDECTESTNIGHTLY_DIR}  KDECTESTNIGHTLY_DIR)
+include( "${KDECTESTNIGHTLY_DIR}/KDECTestNightly.cmake"  OPTIONAL  RESULT_VARIABLE fileIncluded)
 
-# generic support code, sets up everything required:
-include( "${_currentDir}/KDECTestNightly.cmake" )
+if(NOT fileIncluded)
+   message(FATAL_ERROR "Did not find file ${KDECTESTNIGHTLY_DIR}/KDECTestNightly.cmake . 
+Set the environment variable KDECTESTNIGHTLY_DIR  to the directory where this file is located. 
+In KDE svn it is in kdesdk/cmake/modules/ ")
+endif(NOT fileIncluded)
+
+kde_ctest_setup("${CMAKE_CURRENT_LIST_FILE}")
 
 ctest_empty_binary_directory("${CTEST_BINARY_DIRECTORY}")
 ctest_start(Nightly)
 ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}" )
+
+include("${CTEST_SOURCE_DIRECTORY}/CTestConfig.cmake")
+include("${CTEST_SOURCE_DIRECTORY}/CTestCustom.cmake" OPTIONAL)
+
 ctest_configure(BUILD "${CTEST_BINARY_DIRECTORY}" )
 ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}" )
 ctest_test(BUILD "${CTEST_BINARY_DIRECTORY}" )
